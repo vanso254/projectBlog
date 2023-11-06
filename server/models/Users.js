cf7a3fa08c80899f0e582551acdc5affe1d02d97
Evans Oduor <oduorevans9@gmail.com>
2023-11-06 11:36:41 +0300

    Setting up the google Auth api

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,9 +1,35 @@
-const mongoose=require("mongoose")
+const mongoose = require('mongoose');
 
-var googleUsers=new mongoose.Schema({
-    googleId: String,
-    email:String,
-    userName:String
-})
+const userSchema = new mongoose.Schema({
+  googleId: String,
+  userName: String,
+  email: String
+});
 
-module.exports=mongoose.model('GoogleUser',googleUsers)
+userSchema.statics.findOrCreate = async function (profile) {
+    try {
+      const userObj = {
+        googleId: profile.id,
+        userName: profile.displayName,
+        email: profile.emails && profile.emails.length > 0 ? profile.emails[0].value : null,
+      };
+  
+      let user = await this.findOne({ googleId: profile.id });
+  
+      if (user) {
+        return user;
+      } else {
+        user = await this.create(userObj);
+        return user;
+      }
+    } catch (err) {
+      console.error(err);
+      return null; // Handle errors appropriately in your code
+    }
+  };
+  
+
+
+const GoogleUser = mongoose.model('GoogleUser', userSchema);
+
+module.exports = GoogleUser;
--- server/services/googleAuth2.js
+++ server/services/googleAuth2.js
@@ -3,44 +3,19 @@ const passport = require('passport');
 const OAuth2Strategy = require('passport-oauth2').Strategy;
 const GoogleUser = require('../models/Users'); // Import the GoogleUser model
 
-GoogleUser.findOrCreate = async function (profile) {
-  try {
-    const user = await GoogleUser.findOne({ googleId: profile.id });
-    if (user) {
-      // User already exists, return the existing user
-      return user;
-    } else {
-      // Create a new user
-      const newUser = new GoogleUser({
-        googleId: profile.id,
-        email: profile.emails[0].value,
-        userName: profile.displayName,
-        // You can set other fields here...
-      });
-
-      const savedUser = await newUser.save();
-      return savedUser;
-    }
-  } catch (err) {
-    return err; // Return the error
-  }
-}
 
 passport.use(new OAuth2Strategy({
   authorizationURL: 'https://accounts.google.com/o/oauth2/auth',
   tokenURL: 'https://accounts.google.com/o/oauth2/token',
   clientID: process.env.GOOGLE_CLIENT_ID, // Your Google Client ID
   clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Your Google Client Secret
-  callbackURL: 'http://localhost:3000/auth/google/callback', // Your callback URL
+  callbackURL: 'http://localhost:4000/auth/google/callback', // Your callback URL
   scope: 'profile email'
-},
-async function(accessToken, refreshToken, profile) {
-  try {
-    const user = await GoogleUser.findOrCreate(profile);
-    return user;
-  } catch (err) {
-    return err; // Handle the error appropriately
-  }
+}, function(accessToken, refreshToken, profile, cb) {
+  // Call the findOrCreate method on the GoogleUser model
+  GoogleUser.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
 }));
 
 passport.serializeUser(function(user, done) {
